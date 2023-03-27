79f43c2f4e50fa979695fff79d675e2de7a63209
zunky4u24 <odewaleolasunkanmi123@gmail.com>
2023-03-27 07:35:59 -0700

    1-swap.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,11 +4,13 @@
  * print_sign - prints the sign of a number
  * @c: The number to be checked
  * Return: 1 for positive num, -1 for negative num or zero for anything else
+ * Description: printing sign of number
  */
 
 int print_sign(int n)
 
 {
+
 	if (n > 0)
 	{
 	_putchar(43);
